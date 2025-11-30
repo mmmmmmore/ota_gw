4a839d3445dcbe88f459d252d8b108cd2a5891e3
mmmmmmore <maochuneric@gmail.com>
2025-11-30 09:30:00 +0800

    update code structure v1130

--- components/spi/README.md
+++
@@ -1,2 +0,0 @@
-this for spi definition and init function define
-use common gpio for the pin definition
--- handler/ov7670_handler/ov7670_handler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stddef.h>
-#include "esp_err.h"
-
-// Interface to capture and encode a JPEG frame from OV7670
-esp_err_t ov7670_handler_get_jpeg(uint8_t **jpeg_data, size_t *jpeg_size);
