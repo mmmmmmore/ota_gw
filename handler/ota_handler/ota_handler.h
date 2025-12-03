90c7bad5f90baa65f568139193c7f3792aa2ed2e
mmmmmmore <maochuneric@gmail.com>
2025-12-03 13:06:38 +0800

    Update ota_handler.h

--- handler/ota_handler/ota_handler.h
+++ handler/ota_handler/ota_handler.h
@@ -12,13 +12,13 @@ typedef struct {
     int partition;         // PARTITION_A / PARTITION_B
     bool upgrading;        // 是否正在升级
     bool last_result;      // 上次结果：true=success, false=fail
-} client_status_info_t;   // ← 改名，不要叫 client_status_t
+} client_status_info_t;
 
 // 下发任务给指定 Client ECU
 esp_err_t ota_handler_send_task(const char *mac, ota_task_t *task);
 
-// 处理来自 Client ECU 的上报消息
-void ota_handler_process_message(int client_sock, const char *json_str);
+// 处理来自 Client ECU 的进度消息（由 msg_handler 调用）
+void ota_handler_process_progress(int sock, cJSON *root);
 
 // 获取所有 Client 的状态信息（供 webserver 查询）
 client_status_info_t* ota_handler_get_status(int *count);
