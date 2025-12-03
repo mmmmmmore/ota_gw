36adafac83730a1f14c0eaffa66fe9545d9e667c
mmmmmmore <maochuneric@gmail.com>
2025-12-03 14:08:38 +0800

    Update tcp_server.h

--- protocol/tcp_server/tcp_server.h
+++ protocol/tcp_server/tcp_server.h
@@ -3,24 +3,17 @@
 
 #include "esp_err.h"
 
-// 回调函数类型定义
-typedef void (*tcp_server_rx_callback_t)(int client_sock, const char *data);
+// 初始化：启动两个端口监听
+void gw_tcp_servers_init(void);
 
-// 设置接收回调函数
-void tcp_server_set_rx_callback(tcp_server_rx_callback_t cb);
-
-// 启动 TCP Server
+// 启动一个 TCP Server 任务（内部使用）
 esp_err_t tcp_server_start(uint16_t port);
 
-// 发送数据到指定客户端
-esp_err_t tcp_server_send(int client_sock, const char *json_str);
-
-// 设置 OTA Server socket
+// OTA Server socket 管理
 void tcp_server_set_ota_sock(int sock);
-
-// 获取 OTA Server socket
 int tcp_server_get_ota_sock(void);
 
+// 发送 JSON 数据
+esp_err_t tcp_server_send(int client_sock, const char *json_str);
+
 #endif // TCP_SERVER_H
-
-
